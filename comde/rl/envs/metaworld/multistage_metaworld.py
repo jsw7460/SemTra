2411740f7c704c401ee6e02ef9a9fe3515b7566f
jsw7460 <jsw7460@gmail.com>
2023-04-21 13:45:29 +0900

    add chunk

--- comde/rl/envs/metaworld/multistage_metaworld.py
+++ comde/rl/envs/metaworld/multistage_metaworld.py
@@ -8,7 +8,7 @@ from meta_world.get_video import SingleTask
 MW_OBS_DIM = 140
 
 
-class DimFixedMetaWorld(gym.Wrapper):
+class MultiStageMetaWorld(gym.Wrapper):
 	ONEHOT_SKILLS_MAPPING = {
 		"box": 0,
 		"puck": 1,
@@ -22,19 +22,20 @@ class DimFixedMetaWorld(gym.Wrapper):
 
 	SKILL_INDEX_MAPPING = {v: k for k, v in ONEHOT_SKILLS_MAPPING.items()}
 
-	def __init__(self, seed: int, task: List, cfg: Dict = None):
+	def __init__(self, seed: int, task: List, n_target: int, cfg: Dict = None):
 
 		if type(task[0]) == int:
 			for i in range(len(task)):
 				task[i] = self.SKILL_INDEX_MAPPING[task[i]]
 
 		base_env = SingleTask(seed=seed, task=task)
-		super(DimFixedMetaWorld, self).__init__(env=base_env)
+		super(MultiStageMetaWorld, self).__init__(env=base_env)
 
 		"""
 			e.g., skill_list: ["door", "stick", "box", "lever"] 
 				-> skill_list_idx: [2, 4, 1, 5]  
 		"""
+		self.n_target = n_target
 		self.cfg = cfg
 		self.observation_space = gym.spaces.Box(low=-np.inf, high=np.inf, shape=(1, MW_OBS_DIM))
 		self.skill_idx_list = [self.ONEHOT_SKILLS_MAPPING[key] for key in self.skill_list]
@@ -44,3 +45,9 @@ class DimFixedMetaWorld(gym.Wrapper):
 
 	def __str__(self):
 		return self.get_short_str_for_save()
+
+	def step(self, action):
+		obs, reward, done, info = super(MultiStageMetaWorld, self).step(action)
+		if self.env.env.mode == self.n_target:
+			done = True
+		return obs, reward, done, info
