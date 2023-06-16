95b622d10b45c5fcf9b04a6599fe2b832a785574
jsw7460 <jsw7460@gmail.com>
2023-06-16 11:08:41 +0900

    misc

--- scripts/kitchen_eval.sh
+++ scripts/kitchen_eval.sh
@@ -1,16 +1,17 @@
 starts=50000
 n_iter=20
 n_parallel=3
+gpu_idx=0
 
 for ((iter=0; iter<n_iter; iter++)); do
   for ((j=0; j<n_parallel; j++)); do
     offset=$((iter * (5000 * n_parallel)))
     step=$((starts + offset + 5000 * j))
     echo $step
-    python3 eval_comde.py \
+    CUDA_VISIBLE_DEVICES=$gpu_idx python3 eval_comde.py \
     date=2023-06-15 \
-    pretrained_suffix=kt_wind_bigmlp_5dir \
-    save_suffix=kt_wind_bigmlp_5dir \
+    pretrained_suffix=kt_wind_bigmlp_10dir \
+    save_suffix=kitchen_0615_10dir \
     env=kitchen \
     use_optimal_target_skill=True \
     non_functionality=wind \
--- train_comde.py
+++ train_comde.py
@@ -1,6 +1,6 @@
-from jax.config import config
-
-config.update("jax_debug_nans", True)
+# from jax.config import config
+#
+# config.update("jax_debug_nans", True)
 
 import random
 from copy import deepcopy
@@ -22,14 +22,6 @@ from comde.utils.common.normalization import get_observation_statistics
 @hydra.main(version_base=None, config_path="config/train", config_name="comde_base.yaml")
 def program(cfg: DictConfig) -> None:
 	cfg = OmegaConf.to_container(cfg, resolve=True)  # type: Dict[str, Union[str, int, Dict]]
-
-	# pretrained_modules = {}
-	# for prtr in cfg["mode"]["pretrained_modules"]:
-	# 	prtr_module = prtr["module"]
-	# 	prtr_path = prtr["path"]
-	# 	module_cls = get_class(cfg[prtr_module]["_target_"])  # type: Union[type, Type[IJaxSavable]]
-	# 	module_instance = module_cls.load(prtr_path)
-	# 	pretrained_modules[prtr_module] = module_instance
 
 	data_dirs, hdf_files = load_data_paths(cfg)
 
